079bc0d9f36f56ebafe69ee6537d1d3660283f05
Marcelo de Souza <marcelomscode@hotmail.com>
2023-01-09 00:30:22 -0300

    teste

--- src/main/java/com/example/demo/HelloControllerTeste.java
+++ src/main/java/com/example/demo/HelloControllerTeste.java
@@ -5,15 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Moeda;
-import com.example.demo.model.Quadros;
+import com.example.demo.model.QuadrosValores;
 import com.example.demo.repositories.QuadrosRepository;
-import com.example.demo.util.SendEmailCotacao;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import com.example.demo.repositories.QuadrosValoresRepository;
 
 
 @RestController
@@ -21,6 +17,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 public class HelloControllerTeste {
 	
     @Autowired QuadrosRepository quadrosRepository;
+    @Autowired QuadrosValoresRepository quadrosV;
 	
 //	@GetMapping("/pegar")
 //	public Moeda teste(@RequestParam("moeda") String moeda, @RequestParam("emailPara") String emailPara) throws JsonMappingException, JsonProcessingException {
@@ -33,11 +30,10 @@ public class HelloControllerTeste {
 //		
 //	}
 
-	@GetMapping()
-	public String hello() {
-		List<Quadros> q = quadrosRepository.findAll();
-		
-    	return q.get(0).getNome();
-	}
+    @GetMapping
+	  public List<QuadrosValores> hello() {
+		  List<QuadrosValores> v = quadrosV.findAll();
+	  return v; 
+	  }
 	
 }
